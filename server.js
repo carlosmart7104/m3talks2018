1eb23e91a689b03657d056b1c3c32ff20d279569
carlosmart7104 <carlosmart7104@gmail.com>
2018-01-17 06:36:48 -0600

    add heroku deploy files

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-Object.assign=require('object-assign');
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
