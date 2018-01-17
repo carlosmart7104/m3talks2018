5f0e168eaf1494e3d071b0e0608f4f6d3b838a4d
carlosmart7104 <carlosmart7104@gmail.com>
2018-01-17 06:18:26 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', (req, res) => { res.send('M3 Talks - Wellcome To The Future'); });
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Algo malo ha sucedido! :S');
+});
+
+app.listen(port, ip, function () {
+	console.log('Servidor corriendo en http://%s:%s', ip, port);
+});
+
+module.exports = app;
