7ccafe5e6a1ff38589a10a49fa39749fa879512d
carlosmart7104 <carlosmart7104@gmail.com>
2018-01-17 06:28:33 -0600

    EJS

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var app = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => { res.send('M3 Talks - Wellcome To The Future'); });
+
+app.get('/index', (req, res) => { res.render('pages/index'); });
 
 // error handling
 app.use(function(err, req, res, next){
