c1d0ba837d257b53acd96b7b736a29da5f014b13
carlosmart7104 <carlosmart7104@gmail.com>
2018-01-17 17:13:17 -0600

    scroll to - scroll next

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,23 @@
 var smallSizeNavBackground = false;
 
 function scroll_to(el){
+	if (el == undefined) {
+		//console.log($('.navbar').offset().top);
+		if ($('.navbar').offset().top < $('#inicio').offset().top + $('#inicio').height() - 70) {
+			el = '#ponentes';
+		} else if ($('.navbar').offset().top < $('#ponentes').offset().top + $('#ponentes').height() - 70) {
+			el = '#programa';
+		} else if ($('.navbar').offset().top < $('#programa').offset().top + $('#programa').height() - 70) {
+			el = '#patrocinadores';
+		} else if ($('.navbar').offset().top < $('#patrocinadores').offset().top + $('#patrocinadores').height() - 70) {
+			el = '#lugar';
+		} else {
+			el = '#inicio';
+		}
+	}
 	if ($(el).offset()) {
 		$('html, body').animate({
-			scrollTop: $(el).offset().top - 80
+			scrollTop: $(el).offset().top - 70
 		}, 750);
 	} else {
 		console.log('El elemento referenciado no existe o no tiene una propiedad offset');
@@ -17,12 +31,13 @@ function backgroundNavbar() {
 	$('.navbar').css({ backgroundColor: 'rgba(34, 34, 34, 0.90)' });
 
 	if (scrollTop < maxLimit && !smallSizeNavBackground) {
-		var transparencia = scrollTop / maxLimit;
-		$('.navbar').css({ backgroundColor: 'rgba(34, 34, 34, ' + transparencia * 0.90 +')' });
+		var transparencia = scrollTop / maxLimit * 0.90;
+		$('.navbar').css({ backgroundColor: 'rgba(34, 34, 34, ' + transparencia + ')' });
 	}
 }
 
 $(document).ready(()=>{
+
 	backgroundNavbar();
 
 	$(document).on('scroll', backgroundNavbar);
@@ -33,7 +48,7 @@ $(document).ready(()=>{
 
 	$('.navbar-collapse').on('show.bs.collapse', () => {
 		smallSizeNavBackground = true;
-		$('.navbar').css({ backgroundColor: 'rgba(34, 34, 34, 0.90)' });
+		backgroundNavbar();
 	});
 
 	$('.navbar-collapse').on('hide.bs.collapse', () => {
